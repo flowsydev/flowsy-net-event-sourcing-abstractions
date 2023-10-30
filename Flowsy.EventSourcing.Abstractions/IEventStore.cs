d934ca52091756fdd103ec3f2429ffc9c80fe605
Adrian Aviles <adrian8167e@gmail.com>
2023-10-29 20:11:08 -0700

    Add correlationId argument
    Add correlationId argument to methods of IEventStore.

--- Flowsy.EventSourcing.Abstractions/IEventStore.cs
+++ Flowsy.EventSourcing.Abstractions/IEventStore.cs
@@ -17,6 +17,16 @@ public interface IEventStore<TEvent> : IDisposable, IAsyncDisposable
     Task SaveAsync(string key, TEvent @event, CancellationToken cancellationToken);
 
     /// <summary>
+    /// Saves a single event.
+    /// </summary>
+    /// <param name="key">The key associated with the event.</param>
+    /// <param name="event">The event being saved.</param>
+    /// <param name="correlationId">The correlation ID for the event.</param>
+    /// <param name="cancellationToken">The cancellation token for the asynchronous task.</param>
+    /// <returns>An asyncronous task.</returns>
+    Task SaveAsync(string key, TEvent @event, string correlationId, CancellationToken cancellationToken);
+
+    /// <summary>
     /// Saves a list of events.
     /// </summary>
     /// <param name="key">The key associated with the events.</param>
@@ -24,6 +34,16 @@ public interface IEventStore<TEvent> : IDisposable, IAsyncDisposable
     /// <param name="cancellationToken">The cancellation token for the asynchronous task.</param>
     /// <returns>An asyncronous task.</returns>
     Task SaveAsync(string key, IEnumerable<TEvent> events, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Saves a list of events.
+    /// </summary>
+    /// <param name="key">The key associated with the events.</param>
+    /// <param name="events">The events being saved.</param>
+    /// <param name="correlationId">The correlation ID for the event.</param>
+    /// <param name="cancellationToken">The cancellation token for the asynchronous task.</param>
+    /// <returns>An asyncronous task.</returns>
+    Task SaveAsync(string key, IEnumerable<TEvent> events, string correlationId, CancellationToken cancellationToken);
 
     /// <summary>
     /// Loads a list of events associated with the given key.
