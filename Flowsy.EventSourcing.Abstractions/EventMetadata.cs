95228c872b0d9c4d0c3ccf21786fbf9d06ddef2f
Adrian Aviles <adrian8167e@gmail.com>
2023-10-29 20:39:59 -0700

    Add EventName property
    Add EventName property to EventMetadata.

--- Flowsy.EventSourcing.Abstractions/EventMetadata.cs
+++ Flowsy.EventSourcing.Abstractions/EventMetadata.cs
@@ -5,10 +5,16 @@ namespace Flowsy.EventSourcing.Abstractions;
 /// </summary>
 public class EventMetadata
 {
-    public EventMetadata(string typeName)
+    public EventMetadata(string eventName, string typeName)
     {
+        EventName = eventName;
         TypeName = typeName;
     }
+
+    /// <summary>
+    /// The name of the event associated to TypeName.
+    /// </summary>
+    public string EventName { get; }
 
     /// <summary>
     /// Type name used to create instances of event objects when reading them from an event store.
